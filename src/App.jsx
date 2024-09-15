8170c677ba11b94a795bc72b67944e97dc6e925e
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-15 20:52:40 +0530

    added routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import store from "./store/store";
 import { Provider } from "react-redux";
 import Header from "./Header";
 import SideBar from "./Sidebar";
+import { Outlet } from "react-router-dom";
 const App = () => {
   return (
-    <div className="h-full w-full bg-slate-500 px-10 relative">
+    <div className="h-full w-full">
       <Provider store={store}>
-        <SideBar />
         <Header />
-        <Body />
+        <SideBar />
+        <Outlet />
       </Provider>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import WatchPage from "./WatchPage.jsx";
+import Body from "./Body.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
