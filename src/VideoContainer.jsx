375fe3a9127cf2ea873a82e8e8f22c56023402ac
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-17 20:24:11 +0530

    added custom checks before rendering

--- src/VideoContainer.jsx
+++ src/VideoContainer.jsx
@@ -1,18 +1,16 @@
-import useFetch from "./hooks/useFetch";
 import VideoCard from "./VideoCard";
 import { Link } from "react-router-dom";
-import { URL_POPULAR } from "./constant";
 
 const VideoContainer = ({ result }) => {
-  const data = useFetch(URL_POPULAR);
-  // const video = result ? result : data;
-
   return (
     <>
-      {data && (
+      {result && (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-2">
-          {data.map((item) => (
-            <Link to={"/watch?v=" + item.id} key={item.id}>
+          {result.map((item) => (
+            <Link
+              to={`/watch?v=${item.id.videoId ? item.id.videoId : item.id}`}
+              key={item.id.videoId ? item.id.videoId : item.id}
+            >
               <VideoCard {...item} />
             </Link>
           ))}
