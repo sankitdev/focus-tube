4dbe93ee9f65ee2295494acff2961b3c62d0be2f
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-17 20:21:26 +0530

    added Search URL & autoCompleted

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,5 @@
-const API = import.meta.env.VITE_API_KEY;
-export const URL_POPULAR = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=50&regionCode=In&key=${API}`;
+export const API = import.meta.env.VITE_API_KEY;
+export const URL_POPULAR = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=50&regionCode=In`;
+export const SEARCH_URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&q=`;
+export const SEARCH_AUTOCOMPLETE =
+  "https://suggestqueries.google.com/complete/search?client=firefox&q=";
