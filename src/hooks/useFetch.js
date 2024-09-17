860e2372b2468718615c9cb61b2ccc1e51c5f21e
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-17 20:30:41 +0530

    add url, and urlParam as dependency arary

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,13 +4,14 @@ const useFetch = (url, urlParam) => {
   const [data, setData] = useState(null);
   const searchQuery = urlParam || "";
   useEffect(() => {
+    console.log("Inside Fetch");
     const FetchData = async () => {
       const response = await fetch(url + searchQuery + `&key=${API}`);
       const data = await response.json();
       setData(data.items);
     };
     FetchData();
-  }, []);
+  }, [url, urlParam]);
   return data;
 };
 
