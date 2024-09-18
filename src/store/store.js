005a6edad975ff25c5e51216d593ae365fa5ea16
Ankit Singh <ankitsingh635101@gmail.com>
2024-09-18 18:45:31 +0530

    added searchSlice to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuSlice from "./menuSice";
+import SearchCache from "./SearchCache";
 const store = configureStore({
   reducer: {
     app: menuSlice,
+    search: SearchCache,
   },
 });
 
